9aec751a9b0e503bd2ed9e997f5aa8b52cdfcd29
RyuJaeHwi <91930771+RyuJaeHwi@users.noreply.github.com>
2026-01-05 16:28:51 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iterator>
+using namespace std;
+
+int main() {
+    
+    int T;
+    int A, B;
+    
+    cin >> T;
+
+    for (int i = 0; i < T; i++) {
+        cin >> A >> B;
+        cout << A + B << "\n";
+    }
+
+    return 0;
+}
