995f0147a175c101eb62fa994e14fd898ee4338d
RyuJaeHwi <91930771+RyuJaeHwi@users.noreply.github.com>
2026-01-05 17:30:29 +0900

    [Bronze V] Title: 기찍 N, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iterator>
+#include <string>
+using namespace std;
+
+int main() {
+
+    int N;
+
+    cin >> N;
+
+    for (int i = 1; i <= N; N--) {
+        cout << N << "\n";
+    }
+
+    return 0;
+}
