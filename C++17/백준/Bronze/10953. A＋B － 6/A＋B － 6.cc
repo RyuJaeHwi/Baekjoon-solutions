072681e5f49eb5f65cdcfc7f3a00c7b143a4d0e7
RyuJaeHwi <91930771+RyuJaeHwi@users.noreply.github.com>
2026-01-05 16:30:18 +0900

    [Bronze III] Title: A+B - 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/10953. A＋B － 6/A＋B － 6.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iterator>
+using namespace std;
+
+int main() {
+    
+	int T;
+    int A, B;
+	char comma;
+
+	cin >> T;
+
+	for (int i = 0; i < T; i++) {
+		cin >> A >> comma >> B;
+		cout << A + B << "\n";
+	}
+
+	return 0;
+}
