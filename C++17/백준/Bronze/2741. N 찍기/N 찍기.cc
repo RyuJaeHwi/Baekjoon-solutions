bdf72f7c44d3f5f358cb7769e0a567ed5b937d9b
RyuJaeHwi <91930771+RyuJaeHwi@users.noreply.github.com>
2026-01-05 17:26:31 +0900

    [Bronze V] Title: N 찍기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iterator>
+#include <string>
+using namespace std;
+
+int main() {
+
+    int N;
+
+    cin >> N;
+
+    for (int i = 1; i <= N; i++) {
+        cout << i << "\n";
+    }
+
+    return 0;
+}
