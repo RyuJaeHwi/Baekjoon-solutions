8d51c3edb200c871f133cbb7b675d3b4ef6029ae
RyuJaeHwi <91930771+RyuJaeHwi@users.noreply.github.com>
2026-01-05 16:30:03 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iterator>
+using namespace std;
+
+int main() {
+    
+    int A, B;
+
+	while (cin >> A >> B) {
+		if (A == 0 && B == 0) {
+			break;
+		}
+		else {
+			cout << A + B << '\n';
+		}
+	}
+
+	return 0;
+}
